058b286e1c9748bf92ae8262e15c43e690f71789
William Barom Mingardi <wmingardi@uolinc.com>
2020-03-03 16:06:26 -0300

    Create user tests

--- my_api/app/models/user.rb
+++ my_api/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-  validates :cpf, presence: true, uniqueness: { case_sensitive: false },
+  validates :cpf, presence: true, uniqueness: true,
             numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
   has_many :accounts, dependent: :destroy
 end
--- my_api/spec/models/user_spec.rb
+++ my_api/spec/models/user_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it 'is valid when contains cpf, name and email' do
+    user = User.new(name: 'Roberto Carlos Silva',
+                    cpf: '35356575809',
+                    email: 'roberto.carlos@gmail.com')
+    expect(user).to be_valid
+  end
+
+  it 'is not valid when the name is null' do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is not valid when the email is null' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is not valid when the cpf is null' do
+    user = User.new(cpf: nil)
+    user.valid?
+    expect(user.errors[:cpf]).to include("can't be blank")
+  end
+end
+
+describe User do
+  it 'cannot have the same email' do
+    User.new(name: 'Roberto Carlos Silva',
+             cpf: '35356575809',
+             email: 'roberto.carlos@gmail.com').save
+
+    user = User.new(name: 'Roberto Carlos Silva',
+                    cpf: '35356575809',
+                    email: 'roberto.carlos@gmail.com')
+
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+  end
+
+  it 'cannot have the same CPF' do
+    User.new(name: 'Roberto Carlos Silva',
+             cpf: '35356575809',
+             email: 'roberto.carlos@gmail.com').save
+
+    user = User.new(name: 'Roberto Carlos Silva',
+                    cpf: '35356575809',
+                    email: 'roberto.carlos@gmail.com')
+
+    user.valid?
+    expect(user.errors[:cpf]).to include('has already been taken')
+  end
 end
